728111ae4e0dcbd32c461a5a1deacfd718249642
fabianomalves <fabianomalves@protonmail.com>
2020-11-18 21:42:19 -0300

    Criado atributo de classe OLHOS.

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,5 +1,7 @@
 class Pessoa:
-    def __init__(self, *filhos, nome=None, idade=40):
+    olhos = 2
+
+    def __init__(self, *filhos, nome=None, idade=40, ):
         self.idade = idade
         self.nome = nome
         self.filhos = list(filhos)
@@ -20,5 +22,12 @@ if __name__ == '__main__':
         print(filho.nome)
     luciano.sobrenome = 'Ramalho'
     del luciano.filhos
-    print(luciano.__dict__)
+    luciano.olhos = 1
+    del luciano.olhos
     print(fabiano.__dict__)
+    print(luciano.__dict__)
+    Pessoa.olhos = 3
+    print(Pessoa.olhos)
+    print(luciano.olhos)
+    print(fabiano.olhos)
+    print(id(Pessoa.olhos), id(luciano.olhos), id(fabiano.olhos))
